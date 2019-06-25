a72d538a76780c791d804a635e8baac4c2de4f54
yang.weihao <yang.weihao@baonengmotor.com>
2019-06-25 10:20:55 +0800

    add first project Navigation

---
+++ NavigationView/app/src/main/java/com/example/navigationview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.navigationview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity ywh";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: d");
+    }
+}
